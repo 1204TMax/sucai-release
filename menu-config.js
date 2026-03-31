1315747f331151a18ae23b7f6264068b1e9728ab
唐大山-公司 <1030736935@qq.com>
2026-03-31 22:51:04 +0800

    更新页面

--- menu-config.js
+++ menu-config.js
@@ -93,7 +93,7 @@ const MENU_CONFIG = [
 const BOTTOM_SHORTCUTS = [
   {
     name: '大字报裂变',
-    page: 'AI 创作/工作流.html',
+    page: 'AI 创作/图像素材衍生工作流.html',
     icon: '<rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="3" y1="9" x2="21" y2="9"></line><line x1="9" y1="21" x2="9" y2="9"></line>'
   },
   {
